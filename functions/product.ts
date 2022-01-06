5181fcc6acfe04973997f559ff3853f1426ecf1f
Watataku <39507718+watataku8911@users.noreply.github.com>
2022-01-06 18:35:32 +0900

    Update product.ts

--- functions/product.ts
+++ functions/product.ts
@@ -2,7 +2,7 @@ import { genalateRandomFileName } from "./common";
 import { app } from "../firebase/index";
 import {
   getFirestore,
-  doc,
+  collection,
   addDoc,
   Timestamp,
   getDoc,
@@ -15,9 +15,65 @@ export const productList = () => {};
 
 export const product = () => {};
 
-export const productAdd = () => {
+export const productAdd = (
+  fileList: Array<BlobPart[]> | null,
+  product_name: string,
+  product_body: string,
+  price: number,
+  uid: string
+) => {
   const db = getFirestore(app);
   const storage = getStorage(app);
+
+  const timestamp = Timestamp.now();
+
+  let photoURL = [];
+  if (fileList != null) {
+    fileList.forEach((file) => {
+      const blob = new Blob(file, { type: "image/png" });
+      const fileName = genalateRandomFileName();
+
+      const storageRef = ref(storage, "product/" + fileName);
+
+      uploadBytes(storageRef, blob)
+        .then((snapshot) => {
+          if (snapshot) {
+            getDownloadURL(storageRef)
+              .then((url) => {
+                photoURL.push(url);
+              })
+              .catch(() => {
+                throw new Error("アップロード先のURLが取得できませんでした。");
+              });
+          }
+        })
+        .catch(() => {
+          throw new Error("画像がアップロードできませんでした。");
+        });
+    });
+  }
+
+  //photoURL 初期値
+  //photoURL.push("")
+
+  const data = {
+    created_at: timestamp,
+    updated_at: timestamp,
+    productImage: photoURL,
+    productName: product_name,
+    productBody: product_body,
+    price: price,
+    uid: uid,
+  };
+
+  addDoc(collection(db, "products"), data)
+    .then(() => {
+      const router = useRouter();
+      router.push("/mypage/" + uid);
+    })
+    .catch(() => {
+      throw new Error("出品せきませんでした。");
+    });
 };
 
 export const productEdit = () => {
